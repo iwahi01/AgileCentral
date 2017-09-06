944857a8954aa96d0275d6f91d036dcb77a9ddd3
iwahi01 <iwahi01@ca.com>
2017-09-06 17:01:27 +0900

    チェック

--- src/Hello_Java.java
+++ src/Hello_Java.java
@@ -4,14 +4,15 @@ import com.sun.jna.Native;
 //interface hello extends Library {
 interface HelloLib extends Library {
 	HelloLib INSTANCE = (HelloLib) Native.loadLibrary("Hello.dll", HelloLib.class);
-	void Hello();
+	int Hello(int val);
 }
 
 public class Hello_Java {
 	public static void main(String[] args) {
 		System.out.println("呼び出し元｢Hello_Java｣から出力\n");
-//Update for oDE1
+//Update ! DE1
 		HelloLib hello = HelloLib.INSTANCE;
-		hello.Hello();
+		int val = hello.Hello(99);
+		System.out.println("呼び出し先｢Hello｣からの戻り値=" + val + "\n");
   }
 }
